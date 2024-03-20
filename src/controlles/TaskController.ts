9d807f87089eddf4653e0bf93d6438d71f83826e
Leonardo Chiaralo <leonardo.chiaralo@db1.com.br>
2024-03-20 09:01:02 -0300

    feat: adicinando tratamento de erros

--- src/controlles/TaskController.ts
+++ src/controlles/TaskController.ts
@@ -3,33 +3,58 @@ import TaskService from "../services/TaskService";
 
 class TaskController {
   public async createTask(req: Request, res: Response): Promise<Response> {
-    const data = req.body;
-    const task = await TaskService.postTask(data);
-    return res.json(task);
+    try {
+      const data = req.body;
+      const task = await TaskService.postTask(data);
+      return res.json(task);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).send({ error: err });
+    }
   }
 
   public async readTasks(req: Request, res: Response): Promise<Response> {
-    const tasks = await TaskService.getTasks();
-    return res.json(tasks);
+    try {
+      const tasks = await TaskService.getTasks();
+      return res.json(tasks);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).send({ error: err });
+    }
   }
 
   public async updateTask(req: Request, res: Response): Promise<Response> {
-    const id = req.params.id;
-    const data = req.body;
-    const task = await TaskService.putTask(id, data);
-    return res.json(task);
+    try {
+      const id = req.params.id;
+      const data = req.body;
+      const task = await TaskService.putTask(id, data);
+      return res.json(task);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).send({ error: err });
+    }
   }
 
   public async deleteTask(req: Request, res: Response): Promise<Response> {
-    const id = req.params.id;
-    const task = await TaskService.deleteTask(id);
-    return res.json(task);
+    try {
+      const id = req.params.id;
+      const task = await TaskService.deleteTask(id);
+      return res.json(task);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).send({ error: err });
+    }
   }
 
   public async completeTask(req: Request, res: Response): Promise<Response> {
-    const id = req.params.id;
-    const task = await TaskService.patchTask(id);
-    return res.json(task);
+    try {
+      const id = req.params.id;
+      const task = await TaskService.patchTask(id);
+      return res.json(task);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).send({ error: err });
+    }
   }
 }
 
